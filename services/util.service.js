5b6090487c4105e534c912ce5e906cdb36c03410
JoJobani <hvu2309@gmail.com>
2024-07-31 11:48:24 +0300

    Add dates to demo data

--- services/util.service.js
+++ services/util.service.js
@@ -17,48 +17,16 @@ export function debounce(func, timeout = 300) {
 	}
 }
 
-export function getRandomInt(min, max) {
-	min = Math.ceil(min)
-	max = Math.floor(max)
-	return Math.floor(Math.random() * (max - min + 1)) + min
-}
-
 export function generateRandomName() {
 	const names = ['Jhon', 'Wick', 'Strong', 'Dude', 'Yep', 'Hello', 'World', 'Power', 'Goku', 'Super', 'Hi', 'You', 'Are', 'Awesome']
 	const famName = ['star', 'kamikaza', 'family', 'eat', 'some', 'banana', 'brock', 'david', 'gun', 'walk', 'talk', 'car', 'wing', 'yang', 'snow', 'fire']
 	return names[Math.floor(Math.random() * names.length)] + famName[Math.floor(Math.random() * names.length)]
 }
 
-export function generateRandomImg() {
-	//try to get diff img every time
-	return 'pro' + Math.floor(Math.random() * 17 + 1) + '.png'
-}
-
-export function timeAgo(ms = new Date()) {
-	const date = ms instanceof Date ? ms : new Date(ms)
-	const formatter = new Intl.RelativeTimeFormat('en')
-	const ranges = {
-		years: 3600 * 24 * 365,
-		months: 3600 * 24 * 30,
-		weeks: 3600 * 24 * 7,
-		days: 3600 * 24,
-		hours: 3600,
-		minutes: 60,
-		seconds: 1,
-	}
-	const secondsElapsed = (date.getTime() - Date.now()) / 1000
-	for (let key in ranges) {
-		if (ranges[key] < Math.abs(secondsElapsed)) {
-			const delta = secondsElapsed / ranges[key]
-			let time = formatter.format(Math.round(delta), key)
-			if (time.includes('in')) {
-				time = time.replace('in ', '')
-				time = time.replace('ago', '')
-				time += ' ago'
-			}
-			return time //? time : 'Just now'
-		}
-	}
+export function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min)
+    max = Math.floor(max)
+    return Math.floor(Math.random() * (max - min + 1)) + min
 }
 
 export function randomPastTime() {
